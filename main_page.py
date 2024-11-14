5feb8c3153a17f1d29fc62f4eb915f56ebd47467
Valera Kaurov <snazzyvalera@gmail.com>
2024-11-14 18:19:53 +0300

    fix

--- main_page.py
+++ main_page.py
@@ -1,4 +1,4 @@
-import os
+from selenium.webdriver.chrome.options import Options
 
 from faker import Faker
 from selenium import webdriver
@@ -11,30 +11,41 @@ fake = Faker()
 login = fake.user_name()
 password = fake.password(length=10, special_chars=True, digits=True, upper_case=True, lower_case=True)
 
-driver_path = os.path.join(os.getcwd(), 'chromedriver.exe')
-service = Service(driver_path)
-driver = webdriver.Chrome(service=service)
-link = "https://store.steampowered.com/"
-driver.get(link)
-driver.maximize_window()
+LOGIN_BUTTON_LOCATOR = "//a[contains(@class,'global_action_link')]"
+ACCOUNT_NAME_LOCATOR = "//div[contains(@class,'_3BkiHun-mminuTO-Y-zXke')]//input[@type='text']"
+ACCOUNT_PASS_LOCATOR = "//div[contains(@class,'_3BkiHun-mminuTO-Y-zXke')]//input[@type='password']"
+SIGN_BUTTON_LOCATOR = "//button[@type='submit']"
+MESSAGE_ERROR_LOCATOR = "//div[contains(@class,'_1W_6HXiG4JJ0By1qN_0fGZ')]"
+MESSAGE_ERROR = "Pleas check your password and account name and try again."
+TIMEOUT = 10
 
-login_button = driver.find_element(By.XPATH, "//a[contains(@class,'global_action_link')]")
+options = Options()
+driver = webdriver.Chrome(options=options)
+LINK = "https://store.steampowered.com/"
+driver.get(LINK)
+driver.delete_all_cookies()
+
+login_button = WebDriverWait(driver, TIMEOUT).until(
+    EC.element_to_be_clickable((By.XPATH, LOGIN_BUTTON_LOCATOR)))
 login_button.click()
 
-account_name = WebDriverWait(driver, 10).until(
-    EC.element_to_be_clickable((By.XPATH, "//div[@class='_3BkiHun-mminuTO-Y-zXke']//input[@type='text']")))
+account_name = WebDriverWait(driver, TIMEOUT).until(
+    EC.visibility_of_element_located((By.XPATH, ACCOUNT_NAME_LOCATOR)))
 account_name.send_keys(login)
 
-account_pass = driver.find_element(By.XPATH, "//div[@class='_3BkiHun-mminuTO-Y-zXke']//input[@type='password']")
+account_pass = WebDriverWait(driver, TIMEOUT).until(
+    EC.visibility_of_element_located((By.XPATH, ACCOUNT_PASS_LOCATOR)))
 account_pass.send_keys(password)
 
-sign_button = driver.find_element(By.XPATH, "//button[@type='submit']")
+sign_button = WebDriverWait(driver, TIMEOUT).until(
+    EC.element_to_be_clickable((By.XPATH, SIGN_BUTTON_LOCATOR)))
 sign_button.click()
 
-message_error = WebDriverWait(driver, 10).until(
-    EC.text_to_be_present_in_element((By.XPATH, "//div[@class='_1W_6HXiG4JJ0By1qN_0fGZ']"),
-                                     "Please check your password and account name and try again."))
+sign_button = WebDriverWait(driver, TIMEOUT).until(
+    EC.element_to_be_clickable((By.XPATH, SIGN_BUTTON_LOCATOR)))
 
-alert_message_element = driver.find_element(By.XPATH, "//div[@class='_1W_6HXiG4JJ0By1qN_0fGZ']")
-error_text = alert_message_element.text
-print(f"Error text: {error_text}")
+message_error = WebDriverWait(driver, TIMEOUT).until(
+    EC.visibility_of_element_located((By.XPATH, MESSAGE_ERROR_LOCATOR)))
+
+assert MESSAGE_ERROR in message_error.text, f"""Expected :{MESSAGE_ERROR}
+                Actual result:{message_error.text}"""
