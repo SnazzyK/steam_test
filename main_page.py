77c2e79b361ca3d1752508bdcae196b49f57eae2
Valera Kaurov <snazzyvalera@gmail.com>
2024-11-14 21:59:18 +0300

    fix_locators

--- main_page.py
+++ main_page.py
@@ -4,6 +4,7 @@ from faker import Faker
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.expected_conditions import presence_of_element_located
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
@@ -11,12 +12,12 @@ fake = Faker()
 login = fake.user_name()
 password = fake.password(length=10, special_chars=True, digits=True, upper_case=True, lower_case=True)
 
-LOGIN_BUTTON_LOCATOR = "//a[contains(@class,'global_action_link')]"
-ACCOUNT_NAME_LOCATOR = "//div[contains(@class,'_3BkiHun-mminuTO-Y-zXke')]//input[@type='text']"
-ACCOUNT_PASS_LOCATOR = "//div[contains(@class,'_3BkiHun-mminuTO-Y-zXke')]//input[@type='password']"
-SIGN_BUTTON_LOCATOR = "//button[@type='submit']"
-MESSAGE_ERROR_LOCATOR = "//div[contains(@class,'_1W_6HXiG4JJ0By1qN_0fGZ')]"
-MESSAGE_ERROR = "Pleas check your password and account name and try again."
+LOGIN_BUTTON_LOCATOR = (By.XPATH, "//a[contains(@class,'global_action_link')]")
+ACCOUNT_NAME_LOCATOR = (By.XPATH, "//input[@type='text']")
+ACCOUNT_PASS_LOCATOR = (By.XPATH, "//input[@type='password']")
+SIGN_BUTTON_LOCATOR = (By.XPATH, "//button[@type='submit']")
+MESSAGE_ERROR_LOCATOR = (By.XPATH, "//form//div[5]")
+MESSAGE_ERROR = "Please check your password and account name and try again."
 TIMEOUT = 10
 
 options = Options()
@@ -26,26 +27,25 @@ driver.get(LINK)
 driver.delete_all_cookies()
 
 login_button = WebDriverWait(driver, TIMEOUT).until(
-    EC.element_to_be_clickable((By.XPATH, LOGIN_BUTTON_LOCATOR)))
+    EC.element_to_be_clickable(LOGIN_BUTTON_LOCATOR))
 login_button.click()
 
 account_name = WebDriverWait(driver, TIMEOUT).until(
-    EC.visibility_of_element_located((By.XPATH, ACCOUNT_NAME_LOCATOR)))
+    EC.visibility_of_element_located(ACCOUNT_NAME_LOCATOR))
 account_name.send_keys(login)
 
 account_pass = WebDriverWait(driver, TIMEOUT).until(
-    EC.visibility_of_element_located((By.XPATH, ACCOUNT_PASS_LOCATOR)))
+    EC.visibility_of_element_located(ACCOUNT_PASS_LOCATOR))
 account_pass.send_keys(password)
 
 sign_button = WebDriverWait(driver, TIMEOUT).until(
-    EC.element_to_be_clickable((By.XPATH, SIGN_BUTTON_LOCATOR)))
+    EC.element_to_be_clickable(SIGN_BUTTON_LOCATOR))
 sign_button.click()
 
 sign_button = WebDriverWait(driver, TIMEOUT).until(
-    EC.element_to_be_clickable((By.XPATH, SIGN_BUTTON_LOCATOR)))
+    EC.element_to_be_clickable(SIGN_BUTTON_LOCATOR))
 
 message_error = WebDriverWait(driver, TIMEOUT).until(
-    EC.visibility_of_element_located((By.XPATH, MESSAGE_ERROR_LOCATOR)))
+    EC.visibility_of_element_located(MESSAGE_ERROR_LOCATOR))
 
-assert MESSAGE_ERROR in message_error.text, f"""Expected :{MESSAGE_ERROR}
-                Actual result:{message_error.text}"""
+assert MESSAGE_ERROR in message_error.text, f"Expected :{MESSAGE_ERROR}\nActual result:{message_error.text}"
